e03bdf4ee615fe29a3d6e36f51a716727f89999c
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-11 17:19:38 +0600

    Entity Framework Code first approach learning completed

--- Code First Approach- Entity Framework/CRUD_CodeFirstApproach_EFCore/CRUD_CodeFirstApproach_EFCore/Program.cs
+++ Code First Approach- Entity Framework/CRUD_CodeFirstApproach_EFCore/CRUD_CodeFirstApproach_EFCore/Program.cs
@@ -1,8 +1,13 @@
+using CRUD_CodeFirstApproach_EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+string cs = @"server=DESKTOP-51RF4TH\SQLEXPRESS; database=CRUD_CodeFirstApproach_DB;User Id=sa; Password=1996;";
+builder.Services.AddDbContext<Employee_DB_Context>(options => options.UseSqlServer(cs));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
