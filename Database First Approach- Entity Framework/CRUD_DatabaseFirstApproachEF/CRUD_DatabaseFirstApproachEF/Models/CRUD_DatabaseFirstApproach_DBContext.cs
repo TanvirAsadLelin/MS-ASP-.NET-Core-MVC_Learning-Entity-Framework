c99a597232ffc4402294a1511ff0ddd425ffbc55
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-12 12:30:40 +0600

    Entity Framework Database first approach learning part 1 completed

---
+++ Database First Approach- Entity Framework/CRUD_DatabaseFirstApproachEF/CRUD_DatabaseFirstApproachEF/Models/CRUD_DatabaseFirstApproach_DBContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUD_DatabaseFirstApproachEF.Models
+{
+    public partial class CRUD_DatabaseFirstApproach_DBContext : DbContext
+    {
+        public CRUD_DatabaseFirstApproach_DBContext()
+        {
+        }
+
+        public CRUD_DatabaseFirstApproach_DBContext(DbContextOptions<CRUD_DatabaseFirstApproach_DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-51RF4TH\\SQLEXPRESS;Database=CRUD_DatabaseFirstApproach_DB;User Id=sa;Password=1996;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Gender).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Database First Approach- Entity Framework/CRUD_DatabaseFirstApproachEF/CRUD_DatabaseFirstApproachEF/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD_DatabaseFirstApproachEF.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+        public int Age { get; set; }
+        public int Class { get; set; }
+    }
+}
